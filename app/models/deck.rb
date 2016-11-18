8da33894c47ba36c03b8c441991cc18295ace0bd
Scott Thorpe <sthorpe@gmail.com>
2016-11-18 09:36:36 -0800

    make it better

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -2,4 +2,5 @@ class Deck < ApplicationRecord
   has_attached_file :document
   validates_attachment :document, :content_type => { :content_type => %w(application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document) }, default_url: ->(attachment) { 'homer.png' }
   belongs_to :user
+  has_many :shares
 end
--- app/models/share.rb
+++ app/models/share.rb
@@ -1,2 +1,10 @@
 class Share < ApplicationRecord
+  belongs_to :deck
+  before_save :generate_url_code
+
+
+  protected
+    def generate_url_code
+      self.url = (0...8).map { (65 + rand(26)).chr }.join
+    end
 end

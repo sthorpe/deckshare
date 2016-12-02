6aaa46811f1000bb988dd91223be65eb45e33eb3
Scott Thorpe <sthorpe@gmail.com>
2016-12-02 11:16:41 -0800

    Updates to sidekiq

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -75,11 +75,15 @@ class Deck < ApplicationRecord
 
   def build_slides
     if valid?
-      pdf = Magick::ImageList.new("http:#{self.pdf_url}")
-      pdf.each_with_index do |page_img, i|
-        page_img.write "#{Rails.root}/tmp/#{i}_pdf_page.jpg"
-        slides.build(:image =>  File.new("#{Rails.root}/tmp/#{i}_pdf_page.jpg"))
-        FileUtils.rm "#{Rails.root}/tmp/#{i}_pdf_page.jpg"
+      begin
+        pdf = Magick::ImageList.new("http:#{self.pdf_url}")
+        pdf.each_with_index do |page_img, i|
+          page_img.write "#{Rails.root}/tmp/#{i}_pdf_page.jpg"
+          slides.build(:image =>  File.new("#{Rails.root}/tmp/#{i}_pdf_page.jpg"))
+          FileUtils.rm "#{Rails.root}/tmp/#{i}_pdf_page.jpg"
+        end
+      rescue => exception
+        Bugsnag.notify(exception)
       end
       save
     end

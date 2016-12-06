c21b7a169f9835b8efd86385c7ef295dfea47df8
Scott Thorpe <sthorpe@gmail.com>
2016-12-06 12:50:06 -0800

    Adding react autosuggest

---
+++ client/app/bundles/Dogo/components/ContactsSuggest.jsx
@@ -0,0 +1,105 @@
+import React, { PropTypes } from 'react';
+import Autosuggest from 'react-autosuggest';
+
+// const names = [
+//   'Brian',
+//   'Caley',
+//   'Casey',
+//   'Caroline',
+//   'Chris',
+//   'David',
+//   'Misha'
+// ];
+
+// https://developer.mozilla.org/en/docs/Web/JavaScript/Guide/Regular_Expressions#Using_Special_Characters
+const escapeRegexCharacters = str => str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+
+// const getSuggestions = value => {
+//   const escapedValue = escapeRegexCharacters(value.trim());
+//
+//   if (escapedValue === '') {
+//     return [];
+//   }
+//
+//   const regex = new RegExp('^' + escapedValue, 'i');
+//
+//   return contacts.filter(name => regex.test(name));
+// }
+
+const getSuggestionValue = suggestion => suggestion;
+
+const renderSuggestion = suggestion => suggestion;
+
+const renderInputComponent = inputProps => (
+  <div className="inputContainer">
+    <img className="icon" src="https://cdn4.iconfinder.com/data/icons/small-n-flat/24/user-alt-128.png" />
+    <input {...inputProps} />
+  </div>
+);
+
+export default class ContactsSuggest extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: '',
+      suggestions: []
+    };
+  }
+
+  onChange = (event, { newValue, method }) => {
+    this.setState({
+      value: newValue
+    });
+  };
+
+  onSuggestionsFetchRequested = ({ value }) => {
+    this.setState({
+      suggestions: this.getSuggestions(value)
+    });
+  };
+
+  getSuggestions = (value) => {
+    const escapeRegexCharacters = str => str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+    const escapedValue = escapeRegexCharacters(value.trim());
+    if (escapedValue === '') {
+      return [];
+    }
+
+    const regex = new RegExp('^' + escapedValue, 'i');
+
+    var contacts = [];
+    $.each(this.props.contacts, function( index, value ) {
+      contacts.push(value["name"]);
+    });
+
+    return contacts.filter(name => regex.test(name));
+  }
+
+  onSuggestionsClearRequested = () => {
+    this.setState({
+      suggestions: []
+    });
+  };
+
+  render() {
+    const { value, suggestions } = this.state;
+    const inputProps = {
+      placeholder: "Enter a name",
+      value,
+      onChange: this.onChange
+    };
+
+    return (
+      <Autosuggest
+        suggestions={suggestions}
+        onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+        onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+        getSuggestionValue={getSuggestionValue}
+        renderSuggestion={renderSuggestion}
+        inputProps={inputProps}
+        renderInputComponent={renderInputComponent}
+      />
+    );
+  }
+}
--- client/app/bundles/Dogo/startup/DogoApp.jsx
+++ client/app/bundles/Dogo/startup/DogoApp.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactOnRails from 'react-on-rails';
 
-import DeckContainer from '../containers/DeckContainer';
+//import DeckContainer from '../containers/DeckContainer';
+import ContactsSuggest from '../components/ContactsSuggest';
 
 const DogoApp = (props) => (
-  <DeckContainer {...props} />
+  <ContactsSuggest {...props} />
 );
 
 // This is how react_on_rails can see the HelloWorldApp in the browser.

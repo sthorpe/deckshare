e4acf43d373ee88c8024d48968c08ff96e0055de
Scott Thorpe <sthorpe@gmail.com>
2016-12-13 08:44:47 -0800

    Updating with a new key

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -4,7 +4,8 @@ class Contact < ApplicationRecord
 
   def collect_information_on_contact
     if self.email
-      Clearbit.key = 'sk_8152ae059fd7e013e5291314295331f2'
+      #Clearbit.key = 'sk_8152ae059fd7e013e5291314295331f2'
+      Clearbit.key = 'sk_57501e4b49ad381a7f8d12c1b180cdd5'
       @result = Clearbit::Enrichment.find(email: self.email, stream: true)
       self.raw_clearbit_data = @result
       save

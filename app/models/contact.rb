4777ed095e1e909c7f523cd7e43b0e43b768af92
Scott Thorpe <sthorpe@gmail.com>
2016-12-12 21:46:02 -0800

    Fixing a bug

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,8 +2,6 @@ class Contact < ApplicationRecord
   serialize(:raw_clearbit_data, JSON)
   belongs_to :user
 
-  after_save :collect_information_on_contact
-
   def collect_information_on_contact
     if self.email
       Clearbit.key = 'sk_8152ae059fd7e013e5291314295331f2'

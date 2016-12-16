e629af60e56d4dafeaefae96831c3d4b2a0a2613
Scott Thorpe <sthorpe@gmail.com>
2016-12-16 13:54:59 -0800

    Fixing bug

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -86,6 +86,7 @@ class ContactsController < ApplicationController
           text: 'Hello, world'
         }
       }, access_token: 'EAADRoZCKkBjoBAAADzh8MzWYuI1tyQNrxYd81cDZCIHWWGWqUx1LGOBgyb880tPMdQWF1KZB3fVJMpMLbnWYgMos43EJRHxKDCistFxuBSYYDzd1IcR9q5lRzL6ZBcOfcJk3U4ZCZClVcDz4hKZBWPn5ZA58lDXCemiycRUV6tF1jgZDZD')
+    end
   end
 
   def send_message_to_fb_user
